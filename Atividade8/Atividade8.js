cd143302ce0c815829b3f91cf63f1edb2384b6d9
mauriciotozzi <mauricio.tozzi@gmail.com>
2020-10-14 21:47:59 -0300

    Add files via upload

---
+++ Atividade8/Atividade8.js
@@ -0,0 +1,16 @@
+var Aluno1 = new Object()
+Aluno1.Nome = "Mauricio Tozzi"
+Aluno1.ra= "0030481823030"
+
+var Aluno1 = {
+    Nome: "Mauricio Tozzi",
+    ra: "0030481823030",
+}
+
+Aluno1['nome'] = '0030481823030'
+Aluno1['ra'] = 'Mauricio Tozzi'
+
+
+  
+
+
