eb629d2db48d52301bd2e2123b6b58a80bd1bf6a
mauriciotozzi <mauricio.tozzi@gmail.com>
2020-10-28 21:40:42 -0300

    Add files via upload

---
+++ Atividade10/Atividade10.js
@@ -0,0 +1,11 @@
+function changeImage() {
+        document.getElementById("janela").src = "./break.png";
+}
+
+function changeImage2(){
+    document.getElementById("janela").src = "./close.png"
+}
+
+function changeImage3(){
+    document.getElementById("janela").src = "./open.png"
+}
